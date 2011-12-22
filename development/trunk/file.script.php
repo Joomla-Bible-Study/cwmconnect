90088fe3224abd4a541b9f84ebda758dc09cb285
Brent Cordis <skeebrent@gmail.com>
2011-12-22 17:34:31 -0600

    © 2007 - 2011 Joomla Bible Study Team All rights reserved

--- development/trunk/file.script.php
+++ development/trunk/file.script.php
@@ -2,7 +2,7 @@
 /*
  * @version             $Id: file.script.php 72 $
  * @package		com_churchdirectory
- * @copyright           Copyright (C) 2005 - 2011 Joomla Bible Study, All rights reserved.
+ * @copyright           (C) 2007 - 2011 Joomla Bible Study Team All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
