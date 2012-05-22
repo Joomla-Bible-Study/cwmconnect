0493de9d4e414afa00a806080aa2908fc51afeb2
Brent Cordis <skeebrent@gmail.com>
2012-05-22 08:53:40 -0500

    Working up new Overide for Mailing Address. Signed-off-by:Brent Cordis <skeebrent@gmail.com>

--- development/trunk/com_churchdirectory/site/views/churchdirectory/tmpl/default_address.php
+++ development/trunk/com_churchdirectory/site/views/churchdirectory/tmpl/default_address.php
@@ -11,44 +11,79 @@ defined('_JEXEC') or die;
  * jicon-text, jicon-none, jicon-icon
  */
 ?>
-<?php if (($this->params->get('address_check') > 0) && ($this->churchdirectory->address || $this->churchdirectory->suburb || $this->churchdirectory->state || $this->churchdirectory->country || $this->churchdirectory->postcode)) : ?>
+<?php if ($this->churchdirectory->attribs->get('mailingaddress') || $this->churchdirectory->attribs->get('mailingsuburb') || $this->churchdirectory->attribs->get('mailingstate')): ?>
     <div class="churchdirectory-address">
         <?php if ($this->params->get('address_check') > 0) : ?>
             <span class="<?php echo $this->params->get('marker_class'); ?>" >
                 <?php echo $this->params->get('marker_address'); ?>
             </span>
-            <address>
-            <?php endif; ?>
-            <?php if ($this->churchdirectory->address && $this->params->get('show_street_address')) : ?>
-                <span class="churchdirectory-street">
-                    <?php echo nl2br($this->churchdirectory->address); ?>
-                </span>
-            <?php endif; ?>
-            <?php if ($this->churchdirectory->suburb && $this->params->get('show_suburb')) : ?>
-                <span class="churchdirectory-suburb">
-                    <?php echo $this->churchdirectory->suburb; ?>
-                </span>
-            <?php endif; ?>
-            <?php if ($this->churchdirectory->state && $this->params->get('show_state')) : ?>
-                <span class="churchdirectory-state">
-                    <?php echo $this->churchdirectory->state; ?>
-                </span>
-            <?php endif; ?>
-            <?php if ($this->churchdirectory->postcode && $this->params->get('show_postcode')) : ?>
-                <span class="churchdirectory-postcode">
-                    <?php echo $this->churchdirectory->postcode; ?>
-                </span>
-            <?php endif; ?>
-            <?php if ($this->churchdirectory->country && $this->params->get('show_country')) : ?>
-                <span class="churchdirectory-country">
-                    <?php echo $this->churchdirectory->country; ?>
-                </span>
-            <?php endif; ?>
         <?php endif; ?>
-
-        <?php if ($this->params->get('address_check') > 0) : ?>
-        </address>
+        <?php if ($this->churchdirectory->address && $this->params->get('show_street_address')) : ?>
+            <span class="churchdirectory-street">
+                <?php echo nl2br($this->churchdirectory->attribs->get('mailingaddress')); ?>
+            </span>
+        <?php endif; ?>
+        <?php if ($this->churchdirectory->suburb && $this->params->get('show_suburb')) : ?>
+            <span class="churchdirectory-suburb">
+                <?php echo $this->churchdirectory->attribs->get('mailingsuburb'); ?>
+            </span>
+        <?php endif; ?>
+        <?php if ($this->churchdirectory->state && $this->params->get('show_state')) : ?>
+            <span class="churchdirectory-state">
+                <?php echo $this->churchdirectory->attribs->get('mailingstate'); ?>
+            </span>
+        <?php endif; ?>
+        <?php if ($this->churchdirectory->postcode && $this->params->get('show_postcode')) : ?>
+            <span class="churchdirectory-postcode">
+                <?php echo $this->churchdirectory->attribs->get('mailingpostcode'); ?>
+            </span>
+        <?php endif; ?>
+        <?php if ($this->churchdirectory->country && $this->params->get('show_country')) : ?>
+            <span class="churchdirectory-country">
+                <?php echo $this->churchdirectory->attribs->get('mailingcountry'); ?>
+            </span>
+        <?php endif; ?>
     </div>
+<?php else: ?>
+    <?php if (($this->params->get('address_check') > 0) && ($this->churchdirectory->address || $this->churchdirectory->suburb || $this->churchdirectory->state || $this->churchdirectory->country || $this->churchdirectory->postcode)) : ?>
+        <div class="churchdirectory-address">
+            <?php if ($this->params->get('address_check') > 0) : ?>
+                <span class="<?php echo $this->params->get('marker_class'); ?>" >
+                    <?php echo $this->params->get('marker_address'); ?>
+                </span>
+                <address>
+                <?php endif; ?>
+                <?php if ($this->churchdirectory->address && $this->params->get('show_street_address')) : ?>
+                    <span class="churchdirectory-street">
+                        <?php echo nl2br($this->churchdirectory->address); ?>
+                    </span>
+                <?php endif; ?>
+                <?php if ($this->churchdirectory->suburb && $this->params->get('show_suburb')) : ?>
+                    <span class="churchdirectory-suburb">
+                        <?php echo $this->churchdirectory->suburb; ?>
+                    </span>
+                <?php endif; ?>
+                <?php if ($this->churchdirectory->state && $this->params->get('show_state')) : ?>
+                    <span class="churchdirectory-state">
+                        <?php echo $this->churchdirectory->state; ?>
+                    </span>
+                <?php endif; ?>
+                <?php if ($this->churchdirectory->postcode && $this->params->get('show_postcode')) : ?>
+                    <span class="churchdirectory-postcode">
+                        <?php echo $this->churchdirectory->postcode; ?>
+                    </span>
+                <?php endif; ?>
+                <?php if ($this->churchdirectory->country && $this->params->get('show_country')) : ?>
+                    <span class="churchdirectory-country">
+                        <?php echo $this->churchdirectory->country; ?>
+                    </span>
+                <?php endif; ?>
+            <?php endif; ?>
+
+            <?php if ($this->params->get('address_check') > 0) : ?>
+            </address>
+        </div>
+    <?php endif; ?>
 <?php endif; ?>
 
 <?php if ($this->params->get('show_email') || $this->params->get('show_telephone') || $this->params->get('show_fax') || $this->params->get('show_mobile') || $this->params->get('show_webpage')) : ?>
