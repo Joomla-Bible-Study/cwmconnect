ada8e53eddb6bbc3a5a3b8e1509b9664b95902cd
Brent Cordis <skeebrent@gmail.com>
2012-12-06 18:59:06 -0600

    Working up the Install to look better.

--- churchdirectory.script.php
+++ churchdirectory.script.php
@@ -31,17 +31,137 @@ class com_churchdirectoryInstallerScript
 	 * @author Nicholas K. Dionysopoulos
 	 * @var array
 	 */
-	private $installation_queue = array(// modules => { (folder) => { (module) => { (position), (published) } }* }*
-		'modules' => array('admin' => array(), 'site' => array('birthdayanniversary' => 0,)), // plugins => { (folder) => { (element) => (published) }* }*
-		'plugins' => array('finder' => array('finder_churchdirectory' => 1,), 'search' => array('search_churchdirectory' => 0,),), // template => { (folder) => { (element) => (published) }* }*
-		'template' => array('admin' => array(), 'site' => array('churchdirectory_print' => 1,),));
+	private $installation_queue = array( // modules => { (folder) => { (module) => { (position), (published) } }* }*
+		'modules'  => array(
+			'admin' => array(),
+			'site'  => array('birthdayanniversary' => 0,)
+		),
+		// plugins => { (folder) => { (element) => (published) }* }*
+		'plugins'  => array(
+			'finder' => array('churchdirectory' => 1,),
+			'search' => array('churchdirectory' => 0,),
+		),
+		// template => { (folder) => { (element) => (published) }* }*
+		'template' => array(
+			'admin' => array(),
+			'site'  => array('churchdirectory_print' => 1,),
+		)
+	);
 
 	/**
 	 * Varibles to set default params
 	 *
 	 * @var array
 	 */
-	private $param_array = array('protectedaccess' => "8", "churchdirectory_layout" => "_:default", "show_churchdirectory_category" => "hide", "show_churchdirectory_list" => "0", "presentation_style" => "sliders", "show_address_full" => "0", "show_name" => "1", "show_position" => "1", "show_email" => "1", "show_street_address" => "1", "show_suburb" => "1", "show_state" => "1", "show_postcode" => "1", "show_country" => "1", "show_telephone" => "1", "show_mobile" => "1", "show_fax" => "1", "show_webpage" => "1", "show_misc" => "1", "show_image" => "1", "image" => "", "allow_vcard" => "0", "show_articles" => "1", "show_profile" => "1", "show_links" => "1", "linka_name" => "", "linkb_name" => "", "linkc_name" => "", "linkd_name" => "", "linke_name" => "", "churchdirectory_icons" => "0", "icon_address" => "", "icon_email" => "", "icon_telephone" => "", "icon_mobile" => "", "icon_fax" => "", "icon_misc" => "", "category_layout" => "_:default", "show_category_title" => "1", "show_description" => "1", "show_description_image" => "0", "maxLevel" => "-1", "show_empty_categories" => "0", "show_subcat_desc" => "1", "show_cat_items" => "1", "show_base_description" => "1", "maxLevelcat" => "-1", "show_empty_categories_cat" => "0", "show_subcat_desc_cat" => "1", "show_cat_items_cat" => "1", "show_pagination_limit" => "1", "show_headings" => "1", "show_position_headings" => "1", "show_email_headings" => "1", "show_telephone_headings" => "1", "show_mobile_headings" => "0", "show_fax_headings" => "0", "show_suburb_headings" => "0", "show_state_headings" => "0", "show_country_headings" => "0", "show_pagination" => "2", "show_pagination_results" => "1", "initial_sort" => "ordering", "directory_layout" => "_:default", "dr_presentation_style" => "sliders", "dr_churchdirectory_icons" => "0", "dr_show_address_full" => "0", "show_page_title" => "1", "dr_show_position" => "1", "dr_show_email" => "1", "dr_show_street_address" => "1", "dr_show_suburb" => "1", "dr_show_state" => "1", "dr_show_postcode" => "1", "dr_show_country" => "1", "dr_show_telephone" => "1", "dr_show_mobile" => "1", "dr_show_fax" => "1", "dr_show_webpage" => "1", "dr_show_misc" => "1", "dr_show_spouse" => "1", "dr_show_children" => "1", "dr_show_image" => "1", "dr_image" => "", "dr_allow_kml" => "1", "dr_show_articles" => "1", "dr_show_profile" => "1", "dr_show_links" => "1", "dr_linka_name" => "", "dr_linkb_name" => "", "dr_linkc_name" => "", "dr_linkd_name" => "", "dr_linke_name" => "", "rows_per_page" => "3", "items_per_row" => "2", "dinitial_sort" => "lname", "dr_show_debug" => "0", "show_email_form" => "1", "show_email_copy" => "1", "banned_email" => "", "banned_subject" => "", "banned_text" => "", "validate_session" => "1", "custom_reply" => "0", "redirect" => "", "show_feed_link" => "1", "sef_advanced_link" => "0",);
+	private $param_array = array(
+		'protectedaccess'               => "8",
+		"churchdirectory_layout"        => "_:default",
+		"show_churchdirectory_category" => "hide",
+		"show_churchdirectory_list"     => "0",
+		"presentation_style"            => "sliders",
+		"show_address_full"             => "0",
+		"show_name"                     => "1",
+		"show_position"                 => "1",
+		"show_email"                    => "1",
+		"show_street_address"           => "1",
+		"show_suburb"                   => "1",
+		"show_state"                    => "1",
+		"show_postcode"                 => "1",
+		"show_country"                  => "1",
+		"show_telephone"                => "1",
+		"show_mobile"                   => "1",
+		"show_fax"                      => "1",
+		"show_webpage"                  => "1",
+		"show_misc"                     => "1",
+		"show_image"                    => "1",
+		"image"                         => "",
+		"allow_vcard"                   => "0",
+		"show_articles"                 => "1",
+		"show_profile"                  => "1",
+		"show_links"                    => "1",
+		"linka_name"                    => "",
+		"linkb_name"                    => "",
+		"linkc_name"                    => "",
+		"linkd_name"                    => "",
+		"linke_name"                    => "",
+		"churchdirectory_icons"         => "0",
+		"icon_address"                  => "",
+		"icon_email"                    => "",
+		"icon_telephone"                => "",
+		"icon_mobile"                   => "",
+		"icon_fax"                      => "",
+		"icon_misc"                     => "",
+		"category_layout"               => "_:default",
+		"show_category_title"           => "1",
+		"show_description"              => "1",
+		"show_description_image"        => "0",
+		"maxLevel"                      => "-1",
+		"show_empty_categories"         => "0",
+		"show_subcat_desc"              => "1",
+		"show_cat_items"                => "1",
+		"show_base_description"         => "1",
+		"maxLevelcat"                   => "-1",
+		"show_empty_categories_cat"     => "0",
+		"show_subcat_desc_cat"          => "1",
+		"show_cat_items_cat"            => "1",
+		"show_pagination_limit"         => "1",
+		"show_headings"                 => "1",
+		"show_position_headings"        => "1",
+		"show_email_headings"           => "1",
+		"show_telephone_headings"       => "1",
+		"show_mobile_headings"          => "0",
+		"show_fax_headings"             => "0",
+		"show_suburb_headings"          => "0",
+		"show_state_headings"           => "0",
+		"show_country_headings"         => "0",
+		"show_pagination"               => "2",
+		"show_pagination_results"       => "1",
+		"initial_sort"                  => "ordering",
+		"directory_layout"              => "_:default",
+		"dr_presentation_style"         => "sliders",
+		"dr_churchdirectory_icons"      => "0",
+		"dr_show_address_full"          => "0",
+		"show_page_title"               => "1",
+		"dr_show_position"              => "1",
+		"dr_show_email"                 => "1",
+		"dr_show_street_address"        => "1",
+		"dr_show_suburb"                => "1",
+		"dr_show_state"                 => "1",
+		"dr_show_postcode"              => "1",
+		"dr_show_country"               => "1",
+		"dr_show_telephone"             => "1",
+		"dr_show_mobile"                => "1",
+		"dr_show_fax"                   => "1",
+		"dr_show_webpage"               => "1",
+		"dr_show_misc"                  => "1",
+		"dr_show_spouse"                => "1",
+		"dr_show_children"              => "1",
+		"dr_show_image"                 => "1",
+		"dr_image"                      => "",
+		"dr_allow_kml"                  => "1",
+		"dr_show_articles"              => "1",
+		"dr_show_profile"               => "1",
+		"dr_show_links"                 => "1",
+		"dr_linka_name"                 => "",
+		"dr_linkb_name"                 => "",
+		"dr_linkc_name"                 => "",
+		"dr_linkd_name"                 => "",
+		"dr_linke_name"                 => "",
+		"rows_per_page"                 => "3",
+		"items_per_row"                 => "2",
+		"dinitial_sort"                 => "lname",
+		"dr_show_debug"                 => "0",
+		"show_email_form"               => "1",
+		"show_email_copy"               => "1",
+		"banned_email"                  => "",
+		"banned_subject"                => "",
+		"banned_text"                   => "",
+		"validate_session"              => "1",
+		"custom_reply"                  => "0",
+		"redirect"                      => "",
+		"show_feed_link"                => "1",
+		"sef_advanced_link"             => "0",
+	);
 
 	/**
 	 * preflight runs before anything else and while the extracted files are in the uploaded temp folder.
@@ -124,7 +244,8 @@ class com_churchdirectoryInstallerScript
         <thead>
         <tr>
             <th class="title" colspan="2">Extension</th>
-            <th style="width:30%"><?php echo JTEXT::_('COM_CHURCHDIRECTORY_STATUS'); ?></th>
+            <th style="width="
+            30%"><?php echo JTEXT::_('COM_CHURCHDIRECTORY_STATUS'); ?></th>
         </tr>
         </thead>
         <tfoot>
@@ -187,7 +308,7 @@ class com_churchdirectoryInstallerScript
         <thead>
         <tr>
             <th class="title" colspan="2"><?php echo JText::_('COM_CHURCHDIRECTORY_EXTENSION'); ?></th>
-            <th style="width:30%"><?php echo JText::_('COM_CHURCHDIRECTORY_STATUS'); ?></th>
+            <th width="30%"><?php echo JText::_('COM_CHURCHDIRECTORY_STATUS'); ?></th>
         </tr>
         </thead>
         <tfoot>
@@ -226,8 +347,7 @@ class com_churchdirectoryInstallerScript
             <tr class="row<?php echo (++$rows % 2); ?>">
                 <td class="key"><?php echo ucfirst($plugin['name']); ?></td>
                 <td class="key"><?php echo ucfirst($plugin['group']); ?></td>
-                <td><strong
-                        style="color: <?php echo ($plugin['result']) ? "green" : "red" ?>"><?php echo ($plugin['result']) ? JText::_('COM_CHURCHDIRECTORY_REMOVED') : JText::_('COM_CHURCHDIRECTORY_NOT_REMOVED'); ?></strong>
+                <td><strong style="color: <?php echo ($plugin['result']) ? "green" : "red" ?>"><?php echo ($plugin['result']) ? JText::_('COM_CHURCHDIRECTORY_REMOVED') : JText::_('COM_CHURCHDIRECTORY_NOT_REMOVED'); ?></strong>
                 </td>
             </tr>
 				<?php endforeach; ?>
@@ -250,7 +370,7 @@ class com_churchdirectoryInstallerScript
 
 		$db = JFactory::getDbo();
 
-		$status = new JObject();
+		$status          = new JObject();
 		$status->modules = array();
 		$status->plugins = array();
 
@@ -274,10 +394,14 @@ class com_churchdirectoryInstallerScript
 					// Was the module already installed?
 					$sql = $db->getQuery(true)->select('COUNT(*)')->from('#__modules')->where($db->qn('module') . ' = ' . $db->q('mod_' . $module));
 					$db->setQuery($sql);
-					$count = $db->loadResult();
-					$installer = new JInstaller;
-					$result = $installer->install($path);
-					$status->modules[] = array('name' => 'mod_' . $module, 'client' => $folder, 'result' => $result);
+					$count             = $db->loadResult();
+					$installer         = new JInstaller;
+					$result            = $installer->install($path);
+					$status->modules[] = array(
+						'name'   => 'mod_' . $module,
+						'client' => $folder,
+						'result' => $result
+					);
 					// Modify where it's published and its published state
 					if (!$count) {
 						// A. Position and state
@@ -316,9 +440,12 @@ class com_churchdirectoryInstallerScript
 						$query->select('*')->from($db->qn('#__modules_menu'))->where($db->qn('moduleid') . ' = ' . $db->q($moduleid));
 						$db->setQuery($query);
 						$assignments = $db->loadObjectList();
-						$isAssigned = !empty($assignments);
+						$isAssigned  = !empty($assignments);
 						if (!$isAssigned) {
-							$o = (object)array('moduleid' => $moduleid, 'menuid' => 0);
+							$o = (object) array(
+								'moduleid' => $moduleid,
+								'menuid'   => 0
+							);
 							$db->insertObject('#__modules_menu', $o);
 						}
 					}
@@ -348,9 +475,13 @@ class com_churchdirectoryInstallerScript
 					$count = $db->loadResult();
 
 					$installer = new JInstaller;
-					$result = $installer->install($path);
+					$result    = $installer->install($path);
 
-					$status->plugins[] = array('name' => 'plg_' . $plugin, 'group' => $folder, 'result' => $result);
+					$status->plugins[] = array(
+						'name'   => 'plg_' . $plugin,
+						'group'  => $folder,
+						'result' => $result
+					);
 
 					if ($published && !$count) {
 						$query = $db->getQuery(true)->update($db->qn('#__extensions'))->set($db->qn('enabled') . ' = ' . $db->q('1'))->where($db->qn('element') . ' = ' . $db->q($plugin))->where($db->qn('folder') . ' = ' . $db->q($folder));
@@ -377,7 +508,7 @@ class com_churchdirectoryInstallerScript
 
 		$db = JFactory::getDBO();
 
-		$status = new JObject();
+		$status          = new JObject();
 		$status->modules = array();
 		$status->plugins = array();
 
@@ -393,9 +524,13 @@ class com_churchdirectoryInstallerScript
 					$id = $db->loadResult();
 					// Uninstall the module
 					if ($id) {
-						$installer = new JInstaller;
-						$result = $installer->uninstall('module', $id, 1);
-						$status->modules[] = array('name' => 'mod_' . $module, 'client' => $folder, 'result' => $result);
+						$installer         = new JInstaller;
+						$result            = $installer->uninstall('module', $id, 1);
+						$status->modules[] = array(
+							'name'   => 'mod_' . $module,
+							'client' => $folder,
+							'result' => $result
+						);
 					}
 				}
 			}
@@ -410,9 +545,13 @@ class com_churchdirectoryInstallerScript
 
 					$id = $db->loadResult();
 					if ($id) {
-						$installer = new JInstaller;
-						$result = $installer->uninstall('plugin', $id, 1);
-						$status->plugins[] = array('name' => 'plg_' . $plugin, 'group' => $folder, 'result' => $result);
+						$installer         = new JInstaller;
+						$result            = $installer->uninstall('plugin', $id, 1);
+						$status->plugins[] = array(
+							'name'   => 'plg_' . $plugin,
+							'group'  => $folder,
+							'result' => $result
+						);
 					}
 				}
 			}
@@ -459,7 +598,7 @@ class com_churchdirectoryInstallerScript
 			$params = json_decode($db->loadResult(), true);
 			// add the new variable(s) to the existing one(s)
 			foreach ($this->param_array as $name => $value) {
-				$params[(string)$name] = (string)$value;
+				$params[(string) $name] = (string) $value;
 			}
 			// store the combined new and existing values back as a JSON string
 			$paramsString = json_encode($params);
@@ -477,10 +616,21 @@ class com_churchdirectoryInstallerScript
 		// Create categories for our component
 		$basePath = JPATH_ADMINISTRATOR . '/components/com_categories';
 		require_once $basePath . '/models/category.php';
-		$config = array('table_path' => $basePath . '/tables');
+		$config   = array('table_path' => $basePath . '/tables');
 		$catmodel = new CategoriesModelCategory($config);
-		$catData = array('id' => 0, 'parent_id' => 0, 'level' => 1, 'path' => 'default-team', 'extension' => 'com_churchdirectory', 'title' => 'Default Team', 'alias' => 'default-team', 'description' => '<p>Default Team where members are assigned if non are specified.</p>', 'published' => 1, 'language' => '*');
-		$status = $catmodel->save($catData);
+		$catData  = array(
+			'id'          => 0,
+			'parent_id'   => 0,
+			'level'       => 1,
+			'path'        => 'default-team',
+			'extension'   => 'com_churchdirectory',
+			'title'       => 'Default Team',
+			'alias'       => 'default-team',
+			'description' => '<p>Default Team where members are assigned if non are specified.</p>',
+			'published'   => 1,
+			'language'    => '*'
+		);
+		$status   = $catmodel->save($catData);
 
 		if (!$status) {
 			JError::raiseWarning(500, JText::_('Unable to create default ChurchDirectory category!'));
