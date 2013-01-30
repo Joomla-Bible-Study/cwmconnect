74eea67e9b7bad40fa8317a640056141f59a4797
Brent Cordis <skeebrent@gmail.com>
2013-01-30 15:42:51 -0600

    Adding in the fof install

--- churchdirectory.script.php
+++ churchdirectory.script.php
@@ -668,6 +668,118 @@ class Com_ChurchdirectoryInstallerScript
 	}
 
 	/**
+	 * Installs the FOF framework only if the currently installed version on the
+	 * user's site is out of date or if there is no version already installed.
+	 *
+	 * @param JInstaller $parent
+	 *
+	 * @return array
+	 */
+	private function _installFOF($parent)
+	{
+		$src = $parent->getParent()->getPath('source');
+
+		// Install the FOF framework
+		jimport('joomla.filesystem.folder');
+		jimport('joomla.filesystem.file');
+		jimport('joomla.utilities.date');
+		$source = $src . '/fof';
+		if (!defined('JPATH_LIBRARIES'))
+		{
+			$target = JPATH_ROOT . '/libraries/fof';
+		}
+		else
+		{
+			$target = JPATH_LIBRARIES . '/fof';
+		}
+		$haveToInstallFOF = false;
+		if (!JFolder::exists($target))
+		{
+			$haveToInstallFOF = true;
+		}
+		else
+		{
+			$fofVersion = array();
+			if (JFile::exists($target . '/version.txt'))
+			{
+				$rawData                 = JFile::read($target . '/version.txt');
+				$info                    = explode("\n", $rawData);
+				$fofVersion['installed'] = array(
+					'version' => trim($info[0]),
+					'date'    => new JDate(trim($info[1]))
+				);
+			}
+			else
+			{
+				$fofVersion['installed'] = array(
+					'version' => '0.0',
+					'date'    => new JDate('2011-01-01')
+				);
+			}
+			$rawData               = JFile::read($source . '/version.txt');
+			$info                  = explode("\n", $rawData);
+			$fofVersion['package'] = array(
+				'version' => trim($info[0]),
+				'date'    => new JDate(trim($info[1]))
+			);
+
+			$haveToInstallFOF = $fofVersion['package']['date']->toUNIX() > $fofVersion['installed']['date']->toUNIX();
+		}
+
+		$installedFOF = false;
+		if ($haveToInstallFOF)
+		{
+			$versionSource = 'package';
+			$installer     = new JInstaller;
+			$installedFOF  = $installer->install($source);
+		}
+		else
+		{
+			$versionSource = 'installed';
+		}
+
+		if (!isset($fofVersion))
+		{
+			$fofVersion = array();
+			if (JFile::exists($target . '/version.txt'))
+			{
+				$rawData                 = JFile::read($target . '/version.txt');
+				$info                    = explode("\n", $rawData);
+				$fofVersion['installed'] = array(
+					'version' => trim($info[0]),
+					'date'    => new JDate(trim($info[1]))
+				);
+			}
+			else
+			{
+				$fofVersion['installed'] = array(
+					'version' => '0.0',
+					'date'    => new JDate('2011-01-01')
+				);
+			}
+			$rawData               = JFile::read($source . '/version.txt');
+			$info                  = explode("\n", $rawData);
+			$fofVersion['package'] = array(
+				'version' => trim($info[0]),
+				'date'    => new JDate(trim($info[1]))
+			);
+			$versionSource         = 'installed';
+		}
+
+		if (!($fofVersion[$versionSource]['date'] instanceof JDate))
+		{
+			$fofVersion[$versionSource]['date'] = new JDate();
+		}
+
+		return array(
+			'required'  => $haveToInstallFOF,
+			'installed' => $installedFOF,
+			'version'   => $fofVersion[$versionSource]['version'],
+			'date'      => $fofVersion[$versionSource]['date']->format('%Y-%m-%d'),
+		);
+	}
+
+	/**
 	 * Set new Parrams to system.
 	 *
 	 * @return void
