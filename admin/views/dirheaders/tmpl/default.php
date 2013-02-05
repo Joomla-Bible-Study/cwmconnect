5bf05d4122303d05d9bbd827c17b8321ed0e87cf
Brent Cordis <skeebrent@gmail.com>
2013-02-05 09:38:04 -0600

    removed bad code form testing.

--- admin/views/dirheaders/tmpl/default.php
+++ admin/views/dirheaders/tmpl/default.php
@@ -12,7 +12,6 @@ JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 JHtml::_('bootstrap.tooltip');
 JHtml::_('behavior.multiselect');
 JHtml::_('dropdown.init');
-JHtml::_('sortablelist.');
 JHtml::_('formbehavior.chosen', 'select');
 
 $user      = JFactory::getUser();
