135a02c445396dc994d5b11814229fdc91a7e4cb
Brent Cordis <skeebrent@gmail.com>
2013-04-11 14:27:23 -0500

    Close #43 Add Member Birth/Ann to team View plus filter by Month and Show/Hide

--- site/views/category/tmpl/default.php
+++ site/views/category/tmpl/default.php
@@ -23,37 +23,59 @@ else
 ?>
 
 <div class="churchdirectory-category<?php echo $this->pageclass_sfx; ?>">
-	<?php if ($this->params->def('show_page_heading', 1)) : ?>
+	<?php if ($this->params->def('show_page_heading', 1))
+	{
+		?>
 		<div class="page-header">
 			<h1>
 				<?php echo $this->escape($this->params->get('page_heading')); ?>
 			</h1>
 		</div>
-	<?php endif; ?>
-	<?php if ($this->params->get('show_category_title', 1)) : ?>
+	<?php } ?>
+	<?php if ($this->params->get('show_category_title', 1))
+	{
+		?>
 		<h2>
 			<?php echo JHtml::_('content.prepare', $this->category->title); ?>
 		</h2>
-	<?php endif; ?>
-	<div class="pull-right"><a href=<?php echo $href; ?>><?php echo JHTML::_('image.site', 'printButton.png', '/images/M_images/', NULL, NULL, JText::_( 'Print' ) ); ?></a></div>
-	<?php if ($this->params->def('show_description', 1) || $this->params->def('show_description_image', 1)) : ?>
+	<?php } ?>
+	<div class="pull-right">
+		<a href=<?php echo $href; ?>>
+			<?php echo JHTML::_('image.site', 'printButton.png', '/images/M_images/', null, null, JText::_('Print')); ?>
+		</a>
+	</div>
+	<?php if ($this->params->def('show_description', 1) || $this->params->def('show_description_image', 1))
+	{
+		?>
 		<div class="category-desc">
-			<?php if ($this->params->get('show_description_image') && $this->category->getParams()->get('image')) : ?>
+			<?php if ($this->params->get('show_description_image') && $this->category->getParams()->get('image'))
+			{
+				?>
 				<img src="<?php echo $this->category->getParams()->get('image'); ?>"/>
-			<?php endif; ?>
-			<?php if ($this->params->get('show_description') && $this->category->description) : ?>
+			<?php } ?>
+			<?php if ($this->params->get('show_description') && $this->category->description)
+			{
+				?>
 				<?php echo JHtml::_('content.prepare', $this->category->description); ?>
-			<?php endif; ?>
+			<?php } ?>
 			<div class="clr"></div>
 		</div>
-	<?php endif; ?>
+	<?php } ?>
 
 	<?php echo $this->loadTemplate('items'); ?>
 
-	<?php if (!empty($this->children[$this->category->id]) && $this->maxLevel != 0) : ?>
+	<?php if (!empty($this->children[$this->category->id]) && $this->maxLevel != 0)
+	{
+		?>
 		<div class="cat-children">
 			<h3><?php echo JText::_('JGLOBAL_SUBCATEGORIES'); ?></h3>
 			<?php echo $this->loadTemplate('children'); ?>
 		</div>
-	<?php endif; ?>
+	<?php } ?>
+	<div class="clearfix"></div>
+	<?php
+	if ($this->params->def('show_page_birthann', 1))
+	{
+		echo $this->loadTemplate('birthann');
+	}?>
 </div>
