af1fc99013af5515a6c1cfee1c0906ecd00a1ed7
Brent Cordis <skeebrent@gmail.com>
2013-08-24 21:29:19 -0500

    Add safty and include for labraries

--- site/churchdirectory.php
+++ site/churchdirectory.php
@@ -7,6 +7,22 @@
 
 defined('_JEXEC') or die;
 
+// Check for PHP4
+if(defined('PHP_VERSION')) {
+	$version = PHP_VERSION;
+} elseif(function_exists('phpversion')) {
+	$version = phpversion();
+} else {
+// No version info. I'll lie and hope for the best.
+	$version = '5.0.0';
+}
+
+// Old PHP version detected. EJECT! EJECT! EJECT!
+if(!version_compare($version, '5.3.0', '>='))
+{
+	return JError::raise(E_ERROR, 500, 'PHP versions 4.x, 5.0, 5.1 and 5.2 are no longer supported by Church Direcotory.<br/><br/>The version of PHP used on your site is obsolete and contains known security vulenrabilities. Moreover, it is missing features required by Church Directory to work properly or at all. Please ask your host to upgrade your server to the latest PHP 5.3 release. Thank you!');
+}
+
 require_once JPATH_COMPONENT . '/helpers/route.php';
 
 JLoader::register('RenderHelper', JPATH_SITE . '/components/com_churchdirectory/helpers/render.php');
@@ -18,6 +34,12 @@ if (!version_compare(JVERSION, '3.0', 'ge'))
 	JHtml::_('bootstrap.loadcss');
 	JHtml::stylesheet('media/com_churchdirectory/css/bootstrap-j2.5.css');
 }
+
+// Load FOF
+include_once JPATH_SITE.'/libraries/fof/include.php';
+
+// Load tcpdf
+include_once JPATH_SITE.'/libraries/tcpdf/tcpdf.php';
 
 JHTML::stylesheet('media/com_churchdirectory/css/general.css');
 JHTML::stylesheet('media/com_churchdirectory/css/churchdirectory.css');
