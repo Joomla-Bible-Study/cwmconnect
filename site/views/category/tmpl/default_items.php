93ddb3e5fcfad8a16862b86703fe5caaafc02664
Tom Fuller <tomfuller2@gmail.com>
2014-02-28 12:49:29 -0800

    Fixed problem with phone numbers being included in JText. Added parameter to hide photo in category view

--- site/views/category/tmpl/default_items.php
+++ site/views/category/tmpl/default_items.php
@@ -22,24 +22,27 @@ defined('_JEXEC') or die;
 
 			<span class="pull-right">
 				<?php if ($this->params->get('show_telephone_headings') AND !empty($item->telephone)) : ?>
-					<?php echo JTEXT::sprintf('COM_CHURCHDIRECTORY_TELEPHONE_NUMBER'. $item->telephone); ?><br/>
+					<?php echo JTEXT::sprintf('COM_CHURCHDIRECTORY_TELEPHONE_NUMBER'). $item->telephone; ?><br/>
 				<?php endif; ?>
 
 				<?php if ($this->params->get('show_mobile_headings') AND !empty ($item->mobile)) : ?>
-					<?php echo JTEXT::sprintf('COM_CHURCHDIRECTORY_MOBILE_NUMBER'. $item->mobile); ?><br/>
+					<?php echo JTEXT::sprintf('COM_CHURCHDIRECTORY_MOBILE_NUMBER'). $item->mobile; ?><br/>
 				<?php endif; ?>
 
 				<?php if ($this->params->get('show_fax_headings') AND !empty($item->fax)) : ?>
-					<?php echo JTEXT::sprintf('COM_CHURCHDIRECTORY_FAX_NUMBER'. $item->fax); ?><br/>
+					<?php echo JTEXT::sprintf('COM_CHURCHDIRECTORY_FAX_NUMBER'). $item->fax; ?><br/>
 				<?php endif; ?>
 					</span>
 
 			<p>
-			<?php if ($item->image != null) : ?>
-				<?php echo JHtml::image($item->image, JText::_('COM_CHURCHDIRECTORY_IMAGE_DETAILS'), array('align' => 'middle', 'height' => '100px', 'width' => '100px')); ?>
-			<?php else: ?>
-				<?php echo JHtml::image('media/com_churchdirectory/images/200-photo_not_available.jpg', JText::_('COM_CHURCHDIRECTORY_IMAGE_DETAILS'), array('align' => 'middle', 'height' => '100px', 'width' => '100px')); ?>
-			<?php endif; ?><br/>
+            <?php if ($this->params->get('show_image_headings')) : ?>
+                <?php if ( $item->image != null) : ?>
+                    <?php echo JHtml::image($item->image, JText::_('COM_CHURCHDIRECTORY_IMAGE_DETAILS'), array('align' => 'middle', 'height' => '100px', 'width' => '100px')); ?>
+                <?php else: ?>
+                    <?php echo JHtml::image('media/com_churchdirectory/images/200-photo_not_available.jpg', JText::_('COM_CHURCHDIRECTORY_IMAGE_DETAILS'), array('align' => 'middle', 'height' => '100px', 'width' => '100px')); ?>
+                <?php endif; ?>
+            <?php endif; ?>
+            <br/>
 			<strong class="list-title">
 				<a href="<?php echo JRoute::_(ChurchDirectoryHelperRoute::getMemberRoute($item->slug, $item->catid)); ?>">
 					<?php echo $item->name; ?></a>
