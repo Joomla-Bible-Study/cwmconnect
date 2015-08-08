ddbc12cf370638616883ee4dc258c037b1c98d78
Brent Cordis <skeebrent@gmail.com>
2015-08-08 11:22:33 -0500

    Fixes some DB problems with birthdate.

---
+++ admin/sql/updates/mysql/1.7.8.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `#__churchdirectory_details` MODIFY `birthdate` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00';
+ALTER TABLE `#__churchdirectory_details` MODIFY `anniversary` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00';
