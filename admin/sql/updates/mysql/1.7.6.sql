50950fab465ccbe2e4eef21e276e7f5e93543782
bcordis <skeebrent@gmail.com>
2016-09-04 22:24:48 -0500

    Fix sql error for fixing db.

--- admin/sql/updates/mysql/1.7.6.sql
+++ admin/sql/updates/mysql/1.7.6.sql
@@ -1,6 +1,6 @@
-ALTER TABLE `#__churchdirectory_details` ADD COLUMN `mstatus` TINYINT(3) NOT NULL DEFAULT '0'
+ALTER TABLE `#__churchdirectory_details`
+  ADD COLUMN `mstatus` TINYINT(3) NOT NULL DEFAULT '0'
 COMMENT 'Used to track Members Status';
-ALTER TABLE `#__churchdirectory_details` ADD COLUMN `note` VARCHAR(255)
-CHARACTER SET utf8
-COLLATE utf8_general_ci NOT NULL
+ALTER TABLE `#__churchdirectory_details`
+  ADD COLUMN `note` VARCHAR(255) NOT NULL DEFAULT ''
 COMMENT 'Notes on the member for Privet use';
